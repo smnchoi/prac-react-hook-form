c9dc8912462e04a237749944e92d42a089f923c2
Soomin Choi <70505699+smnchoi@users.noreply.github.com>
2023-03-15 23:59:36 +1300

    Fix (1)

--- app/app.tsx
+++ app/app.tsx
@@ -19,6 +19,7 @@ import { AppNavigator, useNavigationPersistence } from "./navigators"
 import { RootStore, RootStoreProvider, setupRootStore } from "./models"
 import { ToggleStorybook } from "../storybook/toggle-storybook"
 import { ErrorBoundary } from "./screens/error/error-boundary"
+import { GestureHandlerRootView } from "react-native-gesture-handler"
 
 // This puts screens in a native ViewController or Activity. If you want fully native
 // stack navigation, use `createNativeStackNavigator` in place of `createStackNavigator`:
@@ -59,10 +60,13 @@ function App() {
       <RootStoreProvider value={rootStore}>
         <SafeAreaProvider initialMetrics={initialWindowMetrics}>
           <ErrorBoundary catchErrors={"always"}>
-            <AppNavigator
-              initialState={initialNavigationState}
-              onStateChange={onNavigationStateChange}
-            />
+            {/* //* YOU MUST ADD THIS. ref: https://docs.swmansion.com/react-native-gesture-handler/docs/installation/ */}
+            <GestureHandlerRootView style={{ flex: 1 }}>
+              <AppNavigator
+                initialState={initialNavigationState}
+                onStateChange={onNavigationStateChange}
+              />
+            </GestureHandlerRootView>
           </ErrorBoundary>
         </SafeAreaProvider>
       </RootStoreProvider>
